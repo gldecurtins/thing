484ae3d205ffcbc4475755105504ce18794c61f6
Gian Luca Decurtins <gldecurtins@icloud.com>
2023-06-28 21:14:38 +0200

    fastApi

--- thing/app/main.py
+++ thing/app/main.py
@@ -1,47 +1,56 @@
+import uvicorn
 import logging
 
-import jinja2
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.staticfiles import StaticFiles
+from faker import Faker
 
-import aiohttp_jinja2
-from aiohttp import web
-from views import index
+log = logging.getLogger(__name__)
 
+app = FastAPI()
 
-async def init_app():
-    app = web.Application()
-
-    app["websockets"] = {}
-
-    app.on_shutdown.append(shutdown)
-
-    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader("templates"))
-
-    app.router.add_get("/", index)
-
-    return app
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 
-async def shutdown(app):
-    for ws in app["websockets"].values():
-        await ws.close()
-    app["websockets"].clear()
+def get_random_name():
+    faker = Faker()
+    return faker.unique.first_name()
 
 
-async def get_app():
-    """Used by aiohttp-devtools for local development."""
-    import aiohttp_debugtoolbar
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
 
-    app = await init_app()
-    aiohttp_debugtoolbar.setup(app)
-    return app
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
 
 
-def main():
-    logging.basicConfig(level=logging.DEBUG)
+manager = ConnectionManager()
 
-    app = init_app()
-    web.run_app(app)
+
+@app.websocket("/")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app, host="0.0.0.0", port=8000)
