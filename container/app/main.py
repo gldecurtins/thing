afe11c8644d48dda9c59b77d006d80c10f89acbc
Gian Luca Decurtins <gldecurtins@icloud.com>
2023-06-29 16:40:42 +0200

    broadcast

--- container/app/main.py
+++ container/app/main.py
@@ -1,3 +1,4 @@
+from typing import TypedDict
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.staticfiles import StaticFiles
 from faker import Faker
@@ -10,23 +11,39 @@ def get_random_name():
     return faker.unique.first_name()
 
 
+class Connection(TypedDict):
+    websocket: WebSocket
+    name: str
+    channel: str
+
+
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: list[WebSocket] = []
+        self.active_connections: dict[Connection] = {}
 
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
-        self.active_connections.append(websocket)
+        self.active_connections[websocket] = get_random_name()
+        text = f">> {self.active_connections[websocket]} connected"
+        await manager.send_personal_message(text, websocket)
+        await manager.send_channel_broadcast(text, websocket)
 
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+    async def disconnect(self, websocket: WebSocket):
+        text = f">> {self.active_connections[websocket]} disconnected"
+        await manager.send_channel_broadcast(text, websocket)
+        del self.active_connections[websocket]
 
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
+    async def send_personal_message(self, text: str, websocket: WebSocket):
+        await websocket.send_text(text)
 
-    async def broadcast(self, message: str):
+    async def send_server_broadcast(self, text: str):
         for connection in self.active_connections:
-            await connection.send_text(message)
+            await connection.send_text(text)
+
+    async def send_channel_broadcast(self, text: str, websocket: WebSocket):
+        for connection in self.active_connections:
+            if connection is not websocket:
+                await connection.send_text(text)
 
 
 manager = ConnectionManager()
@@ -37,12 +54,17 @@ async def websocket_endpoint(websocket: WebSocket):
     await manager.connect(websocket)
     try:
         while True:
-            data = await websocket.receive_text()
-            await manager.send_personal_message(f"{data}", websocket)
-            await manager.broadcast(f"Client says: {data}")
+            text = await websocket.receive_text()
+            if text.startswith("/"):
+                text = f"{manager.active_connections}"
+                await manager.send_personal_message(text, websocket)
+            else:
+                await manager.send_personal_message(text, websocket)
+                await manager.send_channel_broadcast(
+                    f"<{manager.active_connections[websocket]}> {text}", websocket
+                )
     except WebSocketDisconnect:
-        manager.disconnect(websocket)
-        await manager.broadcast(f"Client left the chat")
+        await manager.disconnect(websocket)
 
 
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
